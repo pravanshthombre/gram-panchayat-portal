092868daae36fb6a5f272981e861aa8e3cfa895a
Pravansh Thombre <pravanshthombre@example.com>
2026-04-30 11:44:47 +0530

    Update Vite to output to public directory for Vercel default compatibility

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'static',
+  build: {
+    outDir: 'public',
+    emptyOutDir: true
+  },
   server: {
     port: 3000,
     proxy: {
