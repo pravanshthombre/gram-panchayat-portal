5b7696571f3488450545e951dd893ed26492095f
Pravansh Thombre <pravanshthombre@example.com>
2026-04-30 11:50:05 +0530

    Revert vite config and set vercel.json output directory

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'static',
-  build: {
-    outDir: 'public',
-    emptyOutDir: true
-  },
   server: {
     port: 3000,
     proxy: {
